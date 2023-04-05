c111b1cc6d0df941e01ec13ea60e21420594571b
Vignesh-3005 <vickyvenaik3005@gmail.com>
2023-04-05 21:55:32 +0530

    gitpull1

---
+++ src/org/sample/Sampleprog.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class Sampleprog {
+
+	public void tc_1() {
+		System.out.println("tc1");
+	}
+
+	public void tc_2() {
+		System.out.println("tc2");
+	}
+
+	public void tc_3() {
+		System.out.println("tc3");
+	}
+
+	public void tc_4() {
+		System.out.println("tc4");
+	}
+
+}
